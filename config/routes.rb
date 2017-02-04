1052dfa774017de3869500511037baec7f769ff8
Christian <cabarca250793@gmail.com>
2017-02-04 16:55:34 -0600

    First commit

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+#  get 'pages/home', to: 'pages#home' http://0.0.0.0:3000/pages/home
+  get 'pages/about', to: 'pages#about'
+
+  root 'pages#home' #http://0.0.0.0:3000/
+  get 'about', to: 'pages#about' #http://0.0.0.0:3000/about
+end
