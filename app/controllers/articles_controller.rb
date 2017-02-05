c949f4145314a8fc3027cca41efd0d8691495945
Christian <cabarca250793@gmail.com>
2017-02-05 13:04:08 -0600

    Fix create, add flash and show view

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,8 +7,19 @@ class ArticlesController < ApplicationController
   def create
     #render plain: params[:article].inspect
     @article = Article.new(article_params)
-    @article.save
-    redirect_to articles_show(@article)
+    # @article.save
+    # redirect_to article_path(@article)
+    if @article.save #id save is success
+      #do some
+      flash[:notice] = "Article was successfully created"
+      redirect_to article_path(@article) # it needs the id to render a specific id view
+    else
+      render 'new' # renders the /new page
+    end
+  end
+
+  def show
+    @article = Article.find(params[:id])
   end
 
   private
