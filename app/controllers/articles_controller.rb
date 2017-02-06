a14e64d414b1bb21383a6f017f92807ea7169624
Christian <cabarca250793@gmail.com>
2017-02-05 18:19:30 -0600

    Extract some code

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
 
+before_action :set_article, only: [:edit, :update, :show, :destroy]
+
   def index
     @articles = Article.all
   end
@@ -9,11 +11,9 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def update
-    @article = Article.find(params[:id])
     if @article.update(article_params)
       flash[:notice] = "Article was successfully updated"
       redirect_to article_path(@article) # it needs the id to render a specific id view
@@ -23,7 +23,6 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     flash[:notice] = "Article was successfully deleted"
     redirect_to articles_path
@@ -40,12 +39,15 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    @article = Article.find(params[:id])
   end
 
   private
 
-  def article_params #learn what "whitelist params is"
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params #learn what "whitelist params" is
     params.require(:article).permit(:title, :description)
   end
 end
